8c81bd44fdda17866c49717c4d8f41249e3fa701
claytonweaver <cweaver@cbctrenton.com>
2020-02-06 16:26:39 -0500

    v1.0

---
+++ Library_Mid_Term_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library_Mid_Term_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
