81fdc5f568924c66228a012b5b67a73a61a75f1b
claytonweaver <cweaver@cbctrenton.com>
2020-02-07 11:18:32 -0500

    updated the book class and item class

--- Library_Mid_Term_Project/Item.cs
+++ Library_Mid_Term_Project/Item.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Library_Project_Practice
+namespace Library_Mid_Term_Project
 {
     abstract class Item
     {
         protected string title;
         protected string author;
+        protected string description;
+        protected int numberOfPages;
         protected bool checkedIn;
         protected DateTime dueDate;
 
         public abstract string Title { get; set; }
         public abstract string Author { get; set; }
+        public abstract string Description { get; set; }
+        
         public abstract bool CheckedIn { get; set; }
         public abstract DateTime DueDate { get; set; }
 
@@ -20,10 +24,11 @@ namespace Library_Project_Practice
         {
 
         }
-        public Item(string _title, string _author, bool _checkedIn, DateTime _dueDate)
+        public Item(string _title, string _author, string _description, int _numberOfPages, bool _checkedIn, DateTime _dueDate)
         {
             title = _title;
             author = _author;
+            description = _description;
             checkedIn = _checkedIn;
             dueDate = _dueDate;
         }
