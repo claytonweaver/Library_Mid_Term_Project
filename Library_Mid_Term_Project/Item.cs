b89a9657c4b01d8dd50b322139908882440d1b7d
claytonweaver <cweaver@cbctrenton.com>
2020-02-07 10:12:52 -0500

    added abstract item class

---
+++ Library_Mid_Term_Project/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Project_Practice
+{
+    abstract class Item
+    {
+        protected string title;
+        protected string author;
+        protected bool checkedIn;
+        protected DateTime dueDate;
+
+        public abstract string Title { get; set; }
+        public abstract string Author { get; set; }
+        public abstract bool CheckedIn { get; set; }
+        public abstract DateTime DueDate { get; set; }
+
+        public Item()
+        {
+
+        }
+        public Item(string _title, string _author, bool _checkedIn, DateTime _dueDate)
+        {
+            title = _title;
+            author = _author;
+            checkedIn = _checkedIn;
+            dueDate = _dueDate;
+        }
+
+
+    }
+}
+
+
