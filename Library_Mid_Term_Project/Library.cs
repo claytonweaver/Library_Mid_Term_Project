3a24235d691f9a5a2abaa2286e88fbf8d412980c
Reginald Richardson <youngreg21@gmail.com>
2020-02-07 10:14:38 -0500

    Pushing Reggie Branch.  Code for FileIO to Book List object, Properties and constructor for Books class, and a formatted book inventory file

---
+++ Library_Mid_Term_Project/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Mid_Term_Project
+{
+    class Library
+    {
+
+        //default constructor for Library
+        public Library()
+        {
+
+        }
+
+        public void PrintMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("===================================================");
+            Console.WriteLine("||Welcome to the Detroit City Library Application||");
+            Console.WriteLine("===================================================");
+            Console.WriteLine("How can we help you today:\n     1. Help Check out a Book\n     2. Help Check in a Book\n     3. View Book Inventory");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("     Option: ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        public void UserCheckIn()
+        {
+            //Mark user.status as checked in here
+        }
+
+        public void UserCheckOut()
+        {
+            //Mark user.status as checked out here
+        }
+    }
+}
--- Library_Mid_Term_Project/Program.cs
+++ Library_Mid_Term_Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Library_Mid_Term_Project
 {
@@ -6,7 +8,44 @@ namespace Library_Mid_Term_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //creates an instance of the library application
+            Library session = new Library();
+
+            session.PrintMenu();
+
+
+            //to be changed to type item
+            //title, author, num pages, description
+            List<Books> books = new List<Books>();
+            GetBooks(books);
+
+            foreach(Books book in books)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Title: {book.Title} || Author: {book.Author} || Number of pages: {book.NumPages}");
+                Console.WriteLine("=============================================");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Description: {book.Description}");
+            }
+        }
+
+        //to be changed to get all items (i.e. books, movies, magazines etc...)
+        public static List<Books> GetBooks(List<Books> books)
+        {
+            StreamReader reader = new StreamReader("../../../BookInventory.txt");
+
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                string[] bookInfo = line.Split("|");
+                books.Add(new Books(bookInfo[0], bookInfo[1], int.Parse(bookInfo[2]), bookInfo[3]));
+                line = reader.ReadLine();
+            }
+
+            reader.Close();
+
+            return books;
         }
     }
 }
